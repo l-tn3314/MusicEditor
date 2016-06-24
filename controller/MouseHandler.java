716ef5bbeb50d108e154858183fd626afb3b7384
ssai123 <sbs.sai@gmail.com>
2016-06-24 12:15:40 -0400

    Updated Mouse Handler

--- controller/MouseHandler.java
+++ controller/MouseHandler.java
@@ -3,11 +3,26 @@ package cs3500.music.controller;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MouseHandler implements MouseListener {
+  private Map<Integer, Runnable> mouseClickedMap;
+
+  public MouseHandler() {
+    this.mouseClickedMap = new HashMap<Integer, Runnable>();
+  }
+
+  public void setMouseClickedMap(Map<Integer, Runnable> map) {
+    this.mouseClickedMap = map;
+  }
   @Override
   public void mouseClicked(MouseEvent e) {
-
+    if (e.getButton() == MouseEvent.BUTTON1) {
+      if (this.mouseClickedMap.containsKey(MouseEvent.MOUSE_CLICKED)) {
+        this.mouseClickedMap.get(MouseEvent.MOUSE_CLICKED).run();
+      }
+    }
   }
 
   @Override
